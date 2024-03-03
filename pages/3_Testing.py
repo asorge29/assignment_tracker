a6987785a151d1027a4510f0145573d0deffb4b7
BassMaster629 <124544413+BassMaster629@users.noreply.github.com>
2024-03-02 19:04:50 -0800

    Tile testing

--- pages/3_Testing.py
+++ pages/3_Testing.py
@@ -1,5 +1,16 @@
 import streamlit as st
 from streamlit_card import card
 
-for i in range(10):
-    card(title=str(i), text='This is a card', image='https://picsum.photos/200/300', url='https://www.google.com')
+a = st.columns(4)
+with a[0]:
+    for i in range(10):
+        card(title=str(i), text=['This is a card', "this is a subtext"], image='https://picsum.photos/800/300', url='https://www.google.com', styles={"card":{"width": "100%", "height": "200px", "border-radius": "60px", "background-color": "red"}, 'div':{"padding": "1px"}})
+with a[1]:
+    for i in range(10):
+        card(title=str(i+10), text=['This is a card', "this is a subtext"], image='https://picsum.photos/800/300', url='https://www.google.com', styles={"card":{"width": "100%", "height": "200px", "border-radius": "60px", "background-color": "red"}, 'div':{"padding": "1px"}})
+with a[2]:
+    for i in range(10):
+        card(title=str(i+20), text=['This is a card', "this is a subtext"], image='https://picsum.photos/800/300', url='https://www.google.com', styles={"card":{"width": "100%", "height": "200px", "border-radius": "60px", "background-color": "red"}, 'div':{"padding": "1px"}})
+with a[3]:
+    for i in range(10):
+        card(title=str(i+30), text=['This is a card', "this is a subtext"], image='https://picsum.photos/800/300', url='https://www.google.com', styles={"card":{"width": "100%", "height": "200px", "border-radius": "60px", "background-color": "red"}, 'div':{"padding": "1px"}})
